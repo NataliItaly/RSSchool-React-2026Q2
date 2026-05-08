c938a3d6c7eb5cf1d0dcbfea9c3c27d9316009c2
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-05-09 01:52:41 +0200

    feat: add error boundary test

---
+++ rs-react-app/src/components/ErrorBoundary.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import ErrorBoundary from './ErrorBoundary';
+import BuggyButton from './BuggyButton';
+
+describe('ErrorBoundary component', () => {
+  test('renders children with no error', () => {
+    render(
+      <ErrorBoundary>
+        <div>Test text</div>
+      </ErrorBoundary>
+    );
+
+    expect(screen.getByText('Test text')).toBeInTheDocument();
+  });
+  test('catche error from children component', () => {
+    render(
+      <ErrorBoundary>
+        <div>Test text</div>
+      </ErrorBoundary>
+    );
+
+    fireEvent.click(screen.getByRole('button'));
+
+    expect(screen.getByText(/Something is wrong/i)).toBeInTheDocument();
+    expect(screen.getByText(/Error/i)).toBeInTheDocument();
+  });
+  test('resets error state when clicking Try again', () => {
+    render(
+      <ErrorBoundary>
+        <BuggyButton />
+      </ErrorBoundary>
+    );
+
+    fireEvent.click(screen.getByRole('button'));
+
+    fireEvent.click(screen.getByText(/Try again/i));
+
+    expect(screen.getByText(/Crash App/i)).toBeInTheDocument();
+  });
+});
