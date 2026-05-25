da3f7cda9edb22523021aa1993be9ca094911513
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-05-25 18:25:08 +0200

    feat: add header component tests

--- rs-react-app/src/components/Header/Header.test.tsx
+++ rs-react-app/src/components/Header/Header.test.tsx
@@ -1,6 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import Header from './Header';
 import { ThemeProvider } from '../../context/ThemeProvider';
+import { useTheme } from '../../context/useTheme';
+import { fireEvent } from '@testing-library/react';
+
+vi.mock('../../context/useTheme');
+const mockSetTheme = vi.fn();
+
+beforeEach(() => {
+  vi.clearAllMocks();
+
+  vi.mocked(useTheme).mockReturnValue({
+    theme: 'light',
+    setTheme: mockSetTheme,
+  });
+});
 
 describe('Header component', () => {
   test('renders Header title', () => {
@@ -30,5 +44,25 @@ describe('Header component', () => {
 
     const heading = screen.getByRole('heading', { level: 2 });
     expect(heading).toHaveTextContent('Rick and Morty App');
+  });
+  test('renders theme select with current theme', () => {
+    render(<Header />);
+
+    const select = screen.getByRole('combobox');
+
+    expect(select).toBeInTheDocument();
+    expect(select).toHaveValue('light');
+  });
+  test('calls setTheme when theme changes', () => {
+    render(<Header />);
+
+    const select = screen.getByRole('combobox');
+
+    fireEvent.change(select, {
+      target: { value: 'dark' },
+    });
+
+    expect(mockSetTheme).toHaveBeenCalledWith('dark');
+    expect(mockSetTheme).toHaveBeenCalledTimes(1);
   });
 });
