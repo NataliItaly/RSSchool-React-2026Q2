e7eefd89e511301c4828d16cc7fefbb6b631b32e
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-05-28 19:33:23 +0200

    feat: update store

--- rs-react-app/src/store/index.ts
+++ rs-react-app/src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import selectedItemsReducer from './selectedItemsSlice';
+import { api } from '../api/api';
 
 export const store = configureStore({
   reducer: {
     selectedItems: selectedItemsReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
