a1e830f62da176d6b98f1b3bd049d369e3e526ae
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-05-29 12:43:33 +0200

    refactor: received from api item

--- rs-react-app/src/types/index.ts
+++ rs-react-app/src/types/index.ts
@@ -2,20 +2,10 @@ export interface CardItem {
   id: string | number;
   name: string;
   description?: string;
-  gender?: string;
-  image?: string;
-  species?: string;
-  status?: string;
-  location?: { name: string };
-  url?: string;
-}
-
-export type Character = {
-  id: number;
-  name: string;
-  status: string;
+  gender: string;
   image: string;
   species: string;
-  gender: string;
-  location?: {name: string};
-};
+  status: string;
+  location: { name: string };
+  url: string;
+}
