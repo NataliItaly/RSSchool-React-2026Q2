145e8f32eef1c36bbb2a27f9ee7fb40c97c2644d
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-06-08 23:03:27 +0200

    refactor: delete unused imports

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 // import { Provider } from 'react-redux';
-
-import { setupListeners } from '@reduxjs/toolkit/query';
 import App from './App.tsx';
-
-//setupListeners(store.dispatch);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
